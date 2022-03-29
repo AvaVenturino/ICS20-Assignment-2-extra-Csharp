e16d5a201e89847ba81709374cdbb77b18624c1b
Ava <99355031+AvaVenturino@users.noreply.github.com>
2022-03-29 13:24:22 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,27 @@
+// Created by: Ava Venturino
+// Created on: March 2022
+//
+// This program accepts user input
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        int radius;
+
+        Console.WriteLine("This program finds the circumference of a circle using radius.");
+        Console.WriteLine("");
+        Console.Write("Enter the radius of the circle (cm): ");
+        radius = Convert.ToInt32(Console.ReadLine());
+
+        Double circumference = 2 * (3.1415926 * radius);
+
+        Console.WriteLine("");
+        Console.Write("The circumference of the circle = " + circumference + " cm²");
+        Console.ReadKey();
+        Console.WriteLine("\nDone.");
+    }
+}
